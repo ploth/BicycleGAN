06c92a04cb22af10f1252c348ae12d577b3ae6ba
Pascal Loth <pascal.loth@tuhh.de>
2019-01-30 15:52:04 +0100

    Disable visdom

--- scripts/train_label2packages.sh
+++ scripts/train_label2packages.sh
@@ -14,7 +14,8 @@ NITER_DECAY=200
 
 # training
 GPU_ID=0
-DISPLAY_ID=$((GPU_ID*10+1))
+# DISPLAY_ID=$((GPU_ID*10+1))
+DISPLAY_ID=-1
 PORT=2005
 CHECKPOINTS_DIR=../checkpoints/${CLASS}/
 DATE=`date --iso-8601=seconds`
@@ -23,10 +24,10 @@ NAME=${CLASS}_${MODEL}_${DATE}
 # command
 CUDA_VISIBLE_DEVICES=${GPU_ID} python ./train.py \
   --display_id ${DISPLAY_ID} \
+  --display_port ${PORT} \
   --dataroot ./datasets/${CLASS} \
   --name ${NAME} \
   --model ${MODEL} \
-  #--display_port ${PORT} \
   --direction ${DIRECTION} \
   --checkpoints_dir ${CHECKPOINTS_DIR} \
   --load_size ${LOAD_SIZE} \
