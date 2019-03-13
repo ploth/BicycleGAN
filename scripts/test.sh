a1a89fb6bc803b4b1e3421beac7c6075ed485674
Pascal Loth <pascal.loth@tuhh.de>
2019-03-13 10:22:12 +0100

    Introduce new train and test scripts

---
+++ scripts/test.sh
@@ -0,0 +1,19 @@
+# Path to configuration file is first argument
+source $1
+
+CUDA_VISIBLE_DEVICES=${GPU_ID} python ./test.py \
+  --dataroot ${DATAROOT} \
+  --results_dir ${DEST} \
+  --checkpoints_dir ${CHECKPOINTS_DIR} \
+  --name ${NAME} \
+  --direction ${DIRECTION} \
+  --load_size ${LOAD_SIZE} \
+  --crop_size ${CROP_SIZE} \
+  --nz ${NZ} \
+  --input_nc ${INPUT_NC} \
+  --num_test ${NUM_TEST} \
+  --n_samples ${NUM_SAMPLES} \
+  --aspect_ratio ${ASPECT_RATIO} \
+  --center_crop \
+  --no_flip \
+  --no_encode
