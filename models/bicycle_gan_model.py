4538ef1c1b61051b90d2828e9e5ac1a28502ecc4
Pascal Loth <pascal.loth@tuhh.de>
2019-03-25 11:50:26 +0100

    Introduce weighted inverse L1 loss

--- models/bicycle_gan_model.py
+++ models/bicycle_gan_model.py
@@ -43,6 +43,7 @@ class BiCycleGANModel(BaseModel):
         if opt.isTrain:
             self.criterionGAN = networks.GANLoss(gan_mode=opt.gan_mode).to(self.device)
             self.criterionL1 = torch.nn.L1Loss()
+            self.criterionL2 = torch.nn.MSELoss()
             self.criterionZ = torch.nn.L1Loss()
             # initialize optimizers
             self.optimizers = []
@@ -186,7 +187,7 @@ class BiCycleGANModel(BaseModel):
             self.loss_z_L1 = 0.0
         # penalty for similar images
         if self.opt.lambda_IL1 > 0.0:
-            self.loss_G_IL1 = 1 / self.criterionL1(self.fake_B_random, self.real_B_encoded) * self.opt.lambda_IL1
+            self.loss_G_IL1 = self.criterionL2(self.z_random, self.z_encoded) / self.criterionL1(self.fake_B_random, self.real_B_encoded) * self.opt.lambda_IL1
         else:
             self.loss_G_IL1 = 0.0
 
