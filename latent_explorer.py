0f48b8fc89716886b801618081a1b4d25e2cf514
Pascal Loth <pascal.loth@tuhh.de>
2019-04-03 15:32:57 +0200

    Clean up

--- latent_explorer.py
+++ latent_explorer.py
@@ -19,70 +19,80 @@ from data.aligned_dataset import AlignedDataset
 
 
 class LatentExplorer(QWidget):
-    def __init__(self, model, dataset, opt):
+    def __init__(self, model, opt):
         QWidget.__init__(self)
 
         self.dialog = QFileDialog(self)
         self.dialog.setFileMode(QFileDialog.ExistingFile)
         self.dialog.setNameFilter("Images (*.png *.jpg)")
 
+        # BicycleGAN
         self.model = model
         self.opt = opt
-        self.aligned_dataset = AlignedDataset(opt)
-        self.dataloader = torch.utils.data.DataLoader(
-            self.aligned_dataset, batch_size=1, shuffle=False, num_workers=1)
-        self.dataset = dataset
-        self.input_path = '/home/ploth/projects/pair-images/files/1fd35396a5437cf4397fdfc5dd4b0973c3865f5d_contour.jpg'
+        self.input_path = None
+        self.z = None
 
+        # Buttons
         self.button_load_image = QPushButton("Load image")
-        self.button_generate_random_sample = QPushButton(
-            "Generate random sample")
-        self.z_sample = None
-        self.text = QLabel(self.z_sample)
-        self.text.setAlignment(Qt.AlignCenter)
+        self.button_generate = QPushButton("Generate")
+        self.button_random_z = QPushButton("Random z")
 
+        # Text
+        self.text_input_path = QLabel(self.input_path)
+        self.text_input_path.setAlignment(Qt.AlignCenter)
+        self.text_z = QLabel(self.z)
+        self.text_z.setAlignment(Qt.AlignCenter)
+
+        # Layout
         self.layout = QVBoxLayout()
-        self.layout.addWidget(self.text)
+        self.layout.addWidget(self.text_input_path)
+        self.layout.addWidget(self.text_z)
         self.layout.addWidget(self.button_load_image)
-        self.layout.addWidget(self.button_generate_random_sample)
+        self.layout.addWidget(self.button_random_z)
+        self.layout.addWidget(self.button_generate)
         self.setLayout(self.layout)
 
-        # Connecting the signal
-        self.button_load_image.clicked.connect(self.load_input_image)
-        self.button_generate_random_sample.clicked.connect(
-            self.generate_random_sample)
+        # Connecting the signals
+        self.button_load_image.clicked.connect(self.choose_image)
+        self.button_random_z.clicked.connect(self.generate_random_z)
+        self.button_generate.clicked.connect(self.generate)
 
-    @Slot()
-    def generate_random_sample(self):
-        for i, data in enumerate(islice(self.dataloader, 1)):
-            self.data = data
-            print(data)
-        print('now mine')
-        self.tensor_from_image()
-        #  self.data_from_tensor()
-        print(self.data)
-        self.model.set_input(self.data)
+        # Hacks for testing
+        self.set_input_path('/home/ploth/projects/pair-images/files/1fd35396a5437cf4397fdfc5dd4b0973c3865f5d_contour.jpg')
+        self.load_input_image()
         self.generate_random_z()
+
+    def generate(self):
+        self.model.set_input(self.data)
         self.test()
 
-    def load_input_image(self):
+    def set_input_path(self, path):
+        self.input_path = path
+        self.text_input_path.setText(self.input_path)
+
+    def choose_image(self):
         # Get path from file chooser
         self.dialog.exec_()
-        self.input_path = self.dialog.selectedFiles()
-        print(self.input_path)
+        path = self.dialog.selectedFiles()
+        self.set_input_path(path)
+        self.load_input_image()
+
+    def load_input_image(self):
         # Load image
+        self.image = Image.open(self.input_path).convert('RGB')
         # Convert image to tensor
         self.tensor_from_image()
-        # Construct fake dataset object with A and B tensor an theirs paths
-        # Save dataset element as member
-        #  self.model.set_input(data)
+        # Prepare GAN input
+        self.data_from_tensor()
 
     def tensor_from_image(self):
-        image = Image.open(self.input_path).convert('RGB')
-        parameters = get_params(self.opt, image.size)
+        parameters = get_params(self.opt, self.image.size)
+        #TODO Support for BtoA only!
         transform = get_transform(
             self.opt, parameters, grayscale=(self.opt.input_nc == 1))
-        self.input_tensor = transform(image)
+        tensor = transform(self.image)
+        # Simulate batch of size 1
+        self.input_tensor = torch.unsqueeze(transform(self.image), 0)
 
     def data_from_tensor(self):
         self.data = {
@@ -93,11 +103,11 @@ class LatentExplorer(QWidget):
         }
 
     def generate_random_z(self):
-        self.z_sample = self.model.get_z_random(1, self.opt.nz)
-        self.text.setText(self.z_sample.__str__())
+        self.z = self.model.get_z_random(1, self.opt.nz)
+        self.text_z.setText(self.z.__str__())
 
     def test(self):
-        _, self.fake_B, _ = self.model.test(self.z_sample, encode=False)
+        _, self.fake_B, _ = self.model.test(self.z, encode=False)
         self.np_image = util.tensor2im(self.fake_B)
         util.save_image(self.np_image, './test.png')
 
@@ -112,9 +122,6 @@ if __name__ == "__main__":
     # No shuffle
     opt.serial_batches = True
 
-    # Create dataset
-    dataset = create_dataset(opt)
-
     # Create and setup model
     model = create_model(opt)
     model.setup(opt)
@@ -122,7 +129,7 @@ if __name__ == "__main__":
 
     app = QApplication(sys.argv)
 
-    widget = LatentExplorer(model, dataset, opt)
+    widget = LatentExplorer(model, opt)
     widget.resize(1920, 1080)
     widget.show()
 
